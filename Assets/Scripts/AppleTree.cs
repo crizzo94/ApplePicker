56c26bfe987a0b76f106e0fb16d7add73e399595
crizzo94 <arizzoc@aol.com>
2016-03-06 19:22:30 -0500

    ApplePicker
    Finalized Apple Picker game

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+
+    //Prefab for instatiating apples
+    public GameObject applePrefab;
+
+    //Speed the AppleTree moves in m/s
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 20f;
+
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate Apples will be instatiated
+    public float secondsBetweenAppleDrops = 1f;
+    
+	// Use this for initialization
+	void Start () {
+        //Droppping apples every second
+        InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
+	}
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Basic Movement
+            // defines V3 apos to current position of AppleTree
+        Vector3 pos = transform.position;
+            //x component of pos is increased by the speed times Time.deltaTime (measure of # of secs in last frame)
+        pos.x += speed * Time.deltaTime;
+            //assigns modified pos back to transform.position (moves AppleTree to new pos)
+        transform.position = pos;
+        
+        //Changing Direction
+	    if (pos.x < -leftAndRightEdge)
+        {
+            //move right
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge)
+        {
+            //move left
+            speed = -Mathf.Abs(speed);
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            //change direction
+            speed *= -1;
+        }
+    }
+}
---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Basket : MonoBehaviour {
+
+    public GUIText scoreGT;
+
+	// Update is called once per frame
+	void Update () {
+        //Get the current screen position of the mouse from the Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //The camera's z position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3d = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Move the x position of this Basket to the x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3d.x;
+        this.transform.position = pos;
+
+	}
+    void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<GUIText>();
+        scoreGT.text = "0";
+    }
+    void OnCollisionEnter(Collision coll)
+    {
+        //Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+            if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+        int score = int.Parse(scoreGT.text);
+        score += 100;
+        scoreGT.text = score.ToString();
+
+        if (score > HighScore.score)
+        {
+            HighScore.score = score;
+        }
+    }
+}
